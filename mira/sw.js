c5c5a903ec353ccce7185cfad1992be9c9f14d82
mengluliu90-dot <mengluliu90@gmail.com>
2026-06-26 05:39:39 +0100

    Update Mira service worker cache for v2

--- mira/sw.js
+++ mira/sw.js
@@ -1,4 +1,5 @@
-const CACHE='mira-v2';
+const CACHE='mira-v2-0';
 const FILES=['./','index.html','manifest.json','icon.svg'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));});
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));});
 self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
